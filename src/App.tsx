92511e5326d5e300b92ceded850f126ea21382e4
Sheru Shafiq <humaira.laeeq@gmail.com>
2023-11-22 18:55:17 +0100

    day1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+
 import Header from "./components/Header";
 import sus from "./assets/bg.webp";
+
 const App = () => {
   return (
     <Box
@@ -17,6 +19,35 @@ const App = () => {
       }}
     >
       <Header />
+
+      <Box mt={"15rem"} ml={"40rem"} display={"flex"} flexDirection={"column"}>
+        <Box
+          sx={{
+            width: "10rem",
+            height: "1px",
+            backgroundColor: "white",
+          }}
+        ></Box>
+
+        <Typography
+          mt={"4rem"}
+          color={"white"}
+          fontSize={"72px"}
+          fontWeight={600}
+          lineHeight={"80px"}
+        >
+          Hello brother!
+        </Typography>
+        <Typography
+          mt={"1rem"}
+          color={"#02FFA3"}
+          fontSize={"72px"}
+          fontWeight={600}
+          lineHeight={"80px"}
+        >
+          How does it look?
+        </Typography>
+      </Box>
     </Box>
   );
 };
